c218186b03ac76127d57aa3827f597765d032ecd
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2024-09-24 00:35:41 +0100

    Update app.py

--- Lab 01/app.py
+++ Lab 01/app.py
@@ -5,5 +5,5 @@ app =  Flask(__name__)
 app.register_blueprint(views, url_prefix="/") 
 
 if __name__ == '__main__': # Runs the program by pressing the start button in VS
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
