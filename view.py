17f316a83ee5844c6b37d896fa7b5bb69908d111
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2024-09-23 23:41:24 +0100

    Update view.py

--- view.py
+++ view.py
@@ -4,4 +4,8 @@ views = Blueprint(__name__,"views") # ALlows this py file to be used as a bluepr
 
 @views.route("/")
 def home():
-    return render_template("index.html", name="Thomas") # PAss variables and html files
+    return render_template("index.html", name="Thomas", age=21) # PAss variables and html files
+
+@views.route("/profile/<username>")
+def profile(username):
+    return render_template("index.html",name=username)
