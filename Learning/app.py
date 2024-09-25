5fc2e53a82b2a07f5eb09025158acbaff6c09118
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2024-09-25 09:39:55 +0100

    Created learning website
    Just creating a template, and learning to implement bootstrap with Python Flask

---
+++ Learning/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from view import views # Import the view.py to use the blueprint
+
+app =  Flask(__name__)
+app.register_blueprint(views, url_prefix="/") 
+
+if __name__ == '__main__': # Runs the program by pressing the start button in VS
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
