29da54ab87657b50413d25093b14c0954a0d776c
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2024-09-25 10:07:22 +0100

    Added navbar
    Added navigation bar and stles.css

--- Learning/app.py
+++ Learning/app.py
@@ -1,12 +1,9 @@
 from flask import Flask , url_for, redirect,render_template
-#from views import views # Import the view.py to use the blueprint#
+from views import views # Import the view.py to use the blueprint#
 
 app =  Flask(__name__)
-@app.route("/")
-def home():
-    return render_template("index.html")
 
-#app.register_blueprint(views, url_prefix="/") 
+app.register_blueprint(views, url_prefix="/") 
 
 if __name__ == '__main__': # Runs the program by pressing the start button in VS
     app.run(host='0.0.0.0', port=5000, debug=True)
