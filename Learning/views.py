02b2d6b7656f81fd28ce584d2bfa5558631747d2
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2024-09-25 12:21:02 +0100

    Created About.html

--- Learning/views.py
+++ Learning/views.py
@@ -5,3 +5,7 @@ views = Blueprint('views', __name__)  # Allows this Python file to be used as a 
 @views.route("/")
 def home():
     return render_template("index.html") 
+
+@views.route("/about")
+def about():
+    return render_template("about.html")
