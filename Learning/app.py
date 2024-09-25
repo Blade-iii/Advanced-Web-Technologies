c963ea7d683ab41e76b2e04c845f807cdb99d270
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2024-09-25 09:47:55 +0100

    Index.html & base.html

--- Learning/app.py
+++ Learning/app.py
@@ -1,8 +1,12 @@
-from flask import Flask
-from view import views # Import the view.py to use the blueprint
+from flask import Flask , url_for, redirect,render_template
+#from views import views # Import the view.py to use the blueprint#
 
 app =  Flask(__name__)
-app.register_blueprint(views, url_prefix="/") 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+#app.register_blueprint(views, url_prefix="/") 
 
 if __name__ == '__main__': # Runs the program by pressing the start button in VS
     app.run(host='0.0.0.0', port=5000, debug=True)
