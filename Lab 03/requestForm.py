7854c4b33e5c2d8f07d79bf0d5eaaba8dfb4d5f2
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2024-10-07 16:51:39 +0100

    Added Comments

--- Lab 03/requestForm.py
+++ Lab 03/requestForm.py
@@ -1,27 +1,30 @@
-from flask import Flask, url_for , request
+from flask import Flask, url_for, request  # Import necessary modules from Flask
 
-app = Flask(__name__)
+app = Flask(__name__)  # Create an instance of the Flask class
 
-
-@app.route("/", methods=['GET', 'POST'])
+# Define a route for the root URL "/"
+@app.route("/", methods=['GET', 'POST'])  # The route can accept both GET and POST requests
 def account():
-    if request.method=='POST':
-        print(request.form)
-        name = request.form['name']
-        return "Hello %s" % name
+    # Check if the request method is POST (i.e., the form has been submitted)
+    if request.method == 'POST':
+        print(request.form)  # Print the form data to the console for debugging purposes
+        name = request.form['name']  # Get the value of the 'name' field from the form data
+        return "Hello %s" % name  # Return a greeting message with the name entered in the form
     else:
-        page='''
+        # If the request method is GET, display the HTML form to the user
+        page = '''
         <html>
         <body>
+            <!-- A simple HTML form that sends a POST request back to the same URL -->
             <form action="" method="post" name="form"> 
-            <label for="name">Name:</label>
-            <input type = "text" name="name" id="name"/>
-            <input type="submit" name="submit" id="submit"/> 
+            <label for="name">Name:</label>  <!-- Label for the input field -->
+            <input type = "text" name="name" id="name"/>  <!-- Input field for the user's name -->
+            <input type="submit" name="submit" id="submit"/>  <!-- Submit button to send the form -->
             </form>
         </body>
         </html>
         '''
-    return page
+    return page  # Return the HTML page if the request is not a POST
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)  # Run the Flask app with debug mode enabled, accessible on all IP addresses (host='0.0.0.0')
